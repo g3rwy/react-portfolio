e90a6454e31090c99fbab681ca696b465d3b249c
RechieKho <the.real.richie.kho@gmail.com>
2024-03-08 14:15:37 +0800

    Mention creator name for fame.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(2)
+  const [count, setCount] = useState(2);
 
-  return(
+  return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React by g3rwy</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 4)}>
           count is {count}
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
